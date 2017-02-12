31bc3e5f5c7b5a38ce4e4c9b9f979dbeedd6e29e
jfl110 <typus24@hotmail.com>
2017-02-12 21:54:43 +0000

    Index page, favicon

--- simple-gae-app/src/main/java/org/jfl110/example/AppServletContextListener.java
+++ simple-gae-app/src/main/java/org/jfl110/example/AppServletContextListener.java
@@ -1,10 +1,18 @@
 package org.jfl110.example;
 
+import static org.jfl110.prender.api.HtmlBodyRenderNode.htmlBody;
+import static org.jfl110.prender.api.HtmlPageRenderNode.localHtmlPage;
 import static org.jfl110.prender.api.StringRenderNodes.string;
+import static org.jfl110.prender.api.render.RenderMaps.renderMap;
+import static org.jfl110.prender.api.resources.ServletContextResourceSources.servletContextResource;
 
 import java.util.logging.Logger;
 
+import org.jfl110.prender.api.RenderNode;
+import org.jfl110.prender.api.parse.RenderTag;
+import org.jfl110.prender.api.render.RenderMap;
 import org.jfl110.prender.impl.parse.DefaultParsingModule;
+import org.jfl110.prender.impl.parse.RenderTags;
 import org.jfl110.prender.impl.render.DefaultRenderingModule;
 import org.jfl110.prender.impl.resource.DefaultResourceSourceModule;
 import org.jfl110.prender.impl.serve.Serving;
@@ -19,8 +27,8 @@ import com.google.inject.servlet.ServletModule;
  * 
  * @author JFL110
  */
-public class AppServletContextListener extends GuiceServletContextListener{
-	
+public class AppServletContextListener extends GuiceServletContextListener {
+
 	private static Logger logger = Logger.getLogger(AppServletContextListener.class.getSimpleName());
 
 	@Override
@@ -28,17 +36,31 @@ public class AppServletContextListener extends GuiceServletContextListener{
 		logger.info("Creating injector");
 		return Guice.createInjector(new AppModule());
 	}
-	
-	
-	class AppModule extends ServletModule{
+
+	private static final RenderNode TEMPLATE_PAGE = localHtmlPage("/template.html");
+	private static final RenderNode INDEX_PAGE_CONTENT = htmlBody(servletContextResource("/index-page-content.html"));
+	private static final String TITLE_PLACEHOLDER_KEY = "TITLE";
+	private static final String CONTENT_PLACEHOLDER_KEY = "CONTENT";
+
+	class AppModule extends ServletModule {
 		@Override
 		protected void configureServlets() {
-				
+
 			install(new DefaultRenderingModule());
 			install(new DefaultResourceSourceModule());
 			install(new DefaultParsingModule());
-			
-			install(Serving.of("/").with(string("HELLO!")));
+
+			RenderMap indexPage = renderMap(TEMPLATE_PAGE);
+			indexPage.putPlaceholderValue(TITLE_PLACEHOLDER_KEY, titleTag("Simple GAE App"));
+			indexPage.putPlaceholderValue(CONTENT_PLACEHOLDER_KEY, INDEX_PAGE_CONTENT);
+
+			install(Serving.of("/").with(indexPage));
+			install(Serving.of("/word").with(string("bird")));
+
 		}
+	}
+
+	private RenderTag titleTag(String title) {
+		return RenderTags.tag("title").addChild(string(title)).build();
 	}
 }
